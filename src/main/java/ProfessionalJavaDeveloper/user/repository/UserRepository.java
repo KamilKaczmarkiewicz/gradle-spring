ea5e39a67274db0c788d7da11d161736098b9650
unknown <kaczmarkiewicz98@gmail.com>
2022-06-09 19:45:06 +0200

    n + 1

--- src/main/java/ProfessionalJavaDeveloper/user/repository/UserRepository.java
+++ src/main/java/ProfessionalJavaDeveloper/user/repository/UserRepository.java
@@ -2,12 +2,17 @@ package ProfessionalJavaDeveloper.user.repository;
 
 import ProfessionalJavaDeveloper.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Override
+    @Query("select distinct u from User u join fetch u.characters")
+    List<User> findAll();
 
 }
