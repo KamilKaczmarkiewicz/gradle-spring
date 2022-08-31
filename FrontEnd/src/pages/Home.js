9514cf224f0a8eed9bb7b27cf6221653bddcc458
KamilKaczmarkiewicz <kaczmarkiewicz98@gmail.com>
2022-08-31 15:32:31 +0200

    forgot to push

--- FrontEnd/src/pages/Home.js
+++ FrontEnd/src/pages/Home.js
@@ -1,8 +1,19 @@
+import axios from 'axios'
+
+const getUser = () => {
+    axios.get(`http://localhost:8080/users`, 
+    {withCredentials: true}
+    )
+      .then(res => {
+        console.log(res.data);
+      })
+}
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <button onClick={getUser}>click me</button>
         </div>
     )
   };
